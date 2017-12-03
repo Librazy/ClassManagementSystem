ebc206258baf024ad2da53149a748d3fd64690fc
Librazy <im@librazy.org>
2017-12-03 20:30:38 +0800

    登录页面

---
+++ Controllers/LoginAndRegister/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassManagementSystem.Controllers.LoginAndRegister
+{
+    [Route("")]
+    public class LoginController : Controller
+    {
+        [Route("/Login")]
+        public IActionResult AccountLoginPage()
+        {
+            return View();
+        }
+
+        [Route("/RegisterPage")]
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+    }
+}
